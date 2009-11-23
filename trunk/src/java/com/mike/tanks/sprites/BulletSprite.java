154dbc530411ae04cfd6da35d7f8de57fc67daa7
mjparme <mjparme@2e78b4fc-d650-11de-9f39-755c91dee682>
2009-11-23 23:00:18 +0000

    initial support for firing

--- trunk/src/java/com/mike/tanks/sprites/BulletSprite.java
+++ trunk/src/java/com/mike/tanks/sprites/BulletSprite.java
@@ -1,9 +1,16 @@
 package com.mike.tanks.sprites;
 
+import java.awt.image.BufferedImage;
+
 /**
  * User: mjparme
  * Date: Nov 23, 2009
  * Time: 3:45:33 PM
  */
 public class BulletSprite extends AbstractSprite {
+    public BulletSprite(int x, int y, int gamePanelWidth, int gamePanelHeight, BufferedImage image) {
+        super(x, y, gamePanelWidth, gamePanelHeight, image);
+        deltaX = 4;
+        deltaY = 4;
+    }
 }
